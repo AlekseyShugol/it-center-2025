29bf92123cfd817a037d3cb5190edacb01ed0af0
aleks <aleksshugol2911@gmail.com>
2025-07-27 22:15:03 +0300

    add IOV end

---
+++ 1-helloworld/src/OutputExample2.java
@@ -0,0 +1,4 @@
+public class OutputExample2 {
+    public static void main(String[] args) {
+        System.out.println("Путь к файлу с книгой Барри Бёрда \"Java для чайников\" (7-е издание):\nC:\\books\\java.pdf");    }
+}
